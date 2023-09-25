0a2d63a4a8d3aa5aa38a0f261f3095ff1c93cf56
ededisme <135448961+ededisme@users.noreply.github.com>
2023-09-25 19:59:36 +0700

    My Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Admin Dashboard',
-            'email' => 'admin@admin.com',
-            'password' => '123456789'
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
